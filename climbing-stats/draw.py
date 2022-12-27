667a56e1bc643bb906a1076e84557541b53e30ce
Evgenii Iuliugin <evgenii@Evgeniis-Laptop.lan>
2022-12-27 15:30:39 +0100

    Draw all climbing stats

--- climbing-stats/draw.py
+++ climbing-stats/draw.py
@@ -21,6 +21,8 @@ import matplotlib.pyplot as plt
 
 stats_file = "stats.json"
 
+climbing_styles: list[str] = ['Bouldering', 'Top Rope', 'Lead', 'Moonboard']
+
 with open(stats_file, 'r', encoding='utf8') as f:
     stats = json.load(f)
 
@@ -29,7 +31,8 @@ class Climb():
     grade: str
     result: str
 
-    def __init__(self, stats):
+    def __init__(self, style: str, stats: dict[str, str]):
+        self.style = style
         for key, value in stats.items():
             if key != "name":
                 self.grade = key
@@ -41,67 +44,25 @@ class Climb():
     def flash(self):
         return self.result == 'flash'
 
-class Boulder(Climb):
-    style = 'Bouldering'
-
-class Moon(Climb):
-    style = 'Moonboard'
-
-class TopRope(Climb):
-    style = 'Top-rope'
-
-class Lead(Climb):
-    style = 'Lead'
-
 class ClimbingSession():
     place: str
     date: str
     climbs: list[Climb]
-    bouldering: list[Boulder]
-    lead: list[Lead]
-    top_rope: list[TopRope]
-    moon: list[Moon]
+    styles: list[str]
 
     def __init__(self, stats):
-        for key, value in stats.items():
-            setattr(self, key, ClimbingSession.get_value(key, value))
-
         self.climbs = []
         self.styles = []
-        if hasattr(self, 'bouldering'):
-            self.styles.append('Bouldering')
-            self.climbs.extend(self.bouldering)
-        if hasattr(self, 'top-rope'):
-            self.top_rope = getattr(self, 'top-rope')
-            self.styles.append('Top Rope')
-            self.climbs.extend(self.top_rope)
-        if hasattr(self, 'lead'):
-            self.styles.append('Lead')
-            self.climbs.extend(self.lead)
-        if hasattr(self, 'moon'):
-            self.styles.append('moon')
-            self.climbs.extend(self.moon)
+
+        for key, value in stats.items():
+            if key in climbing_styles:
+                self.styles.append(key)
+                for c in value:
+                    self.climbs.append(Climb(key, c))
+            else:
+                setattr(self, key, value)
 
         assert len(self.styles) != 0
-
-    @staticmethod
-    def get_value(key, value):
-        ret = []
-        if key == 'bouldering':
-            for c in value:
-                ret.append(Boulder(c))
-        elif key == 'top-rope':
-            for c in value:
-                ret.append(TopRope(c))
-        elif key == 'lead':
-            for c in value:
-                ret.append(Lead(c))
-        elif key == 'moon':
-            for c in value:
-                ret.append(Moon(c))
-        else:
-            return value
-        return ret
 
     def __repr__(self):
         ret = 'Session('
@@ -117,6 +78,13 @@ class ClimbingSession():
         ret += ')'
         return ret
 
+    def session_stats(self, style):
+        ret = []
+        for c in self.climbs:
+            if c.style == style:
+                ret.append(c)
+        return ret
+
 sessions: list[ClimbingSession] = []
 for s in stats:
     sessions.append(ClimbingSession(s))
@@ -125,27 +93,24 @@ def plot_data(sessions: list[ClimbingSession], style: str):
     flash: dict[str, int] = {}
     redpoint: dict[str, int] = {}
     for s in sessions:
-        for c in s.climbs:
-            if c.style == style:
-                if c.grade not in flash:
-                    flash[c.grade] = 0
-                    redpoint[c.grade] = 0
+        for c in s.session_stats(style):
+            if c.grade not in flash:
+                flash[c.grade] = 0
+                redpoint[c.grade] = 0
 
-                d = flash if c.flash() else redpoint
-                d[c.grade] += 1
+            d = flash if c.flash() else redpoint
+            d[c.grade] += 1
 
     flash = OrderedDict(sorted(flash.items()))
     redpoint = OrderedDict(sorted(redpoint.items()))
     return (flash, redpoint)
 
-figure, axis = plt.subplots(1, 2)
+figure, axis = plt.subplots(len(climbing_styles), 1)
 
-(f, r) = plot_data(sessions, 'Moonboard')
-axis[0].bar(f.keys(), f.values(), color='green')
-axis[0].bar(r.keys(), r.values(), color='orange', bottom=list(f.values()))
-
-(f, r) = plot_data(sessions, 'Bouldering')
-axis[1].bar(f.keys(), f.values(), color='green')
-axis[1].bar(r.keys(), r.values(), color='orange', bottom=list(f.values()))
+for index, style in enumerate(climbing_styles):
+    axis[index].set_title(f'{style} stats')
+    (f, r) = plot_data(sessions, style)
+    axis[index].bar(list(f.keys()), list(f.values()), color='green')
+    axis[index].bar(list(r.keys()), list(r.values()), color='orange', bottom=list(f.values()))
 
 plt.show()
